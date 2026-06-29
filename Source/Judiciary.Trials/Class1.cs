05547a075ea877b264635a679f1304d763e636bc
Broski <109301662+BitSiphon@users.noreply.github.com>
2026-06-29 15:58:20 -0400

    init new c# project

---
+++ Source/Judiciary.Trials/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace Broski.Judiciary.Trials
+{
+    [StaticConstructorOnStartup]
+    public static class ModMain
+    {
+        static ModMain()
+        {
+            Log.Message("[MyFirstMod] Hello from Linux! The C# assembly has loaded successfully.");
+        }
+    }
+}
